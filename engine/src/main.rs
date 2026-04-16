9a936f2f5555353aa140af628c334374ec3cc87c
Sxmourai <samourai_x@outlook.fr>
2026-04-16 13:26:17 +0200

    fix: large files & gitignore

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -36,7 +36,7 @@ pub use material::*;
 
 fn main() {
     // let world = gen_world();
-    let world = load_world("engine/assets/world/castle.vox").unwrap();
+    let world = load_world("engine/assets/world/sponza.vox").unwrap();
     let root_max_depth = world.root_max_depth();
     let mut app = App::new();
     app
